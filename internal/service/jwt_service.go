f5aeb29cc80abe6decbc823e3f16c124b7e3ec76
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-07-26 09:41:39 +0700

    Updated jwt_service.go to use jwt v5

--- internal/service/jwt_service.go
+++ internal/service/jwt_service.go
@@ -2,9 +2,8 @@ package service
 
 import (
 	"errors"
+	"github.com/golang-jwt/jwt/v5"
 	"time"
-
-	"github.com/golang-jwt/jwt"
 )
 
 type JWTService interface {
@@ -12,56 +11,63 @@ type JWTService interface {
 	ValidateToken(tokenString string) (string, error)
 }
 
-type jwtCustomClaims struct {
-	UserID string `json:"user_id"`
-	jwt.StandardClaims
-}
-
 type jwtService struct {
 	secretKey string
-	issure    string
 }
 
-func NewJWTService(secretKey string, issuer string) JWTService {
+type CustomClaim struct {
+	jwt.RegisteredClaims
+	UserID string `json:"user_id"`
+}
+
+func NewJWTService(secretKey string) JWTService {
 	return &jwtService{
 		secretKey: secretKey,
-		issure:    issuer,
 	}
 }
 
 func (s *jwtService) GenerateToken(userID string) (string, error) {
-	claims := &jwtCustomClaims{
-		userID,
-		jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(time.Hour * 2).Unix(), // 2 hours
-			Issuer:    s.issure,
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, CustomClaim{
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 72)),
 		},
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		UserID: userID,
+	})
 
 	return token.SignedString([]byte(s.secretKey))
 }
 
 func (s *jwtService) ValidateToken(tokenString string) (string, error) {
-	token, err := jwt.ParseWithClaims(
-		tokenString,
-		&jwtCustomClaims{},
-		func(token *jwt.Token) (interface{}, error) {
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, errors.New("unexpected signing method")
-			}
-			return []byte(s.secretKey), nil
-		},
-	)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+
+		return []byte(s.secretKey), nil
+	})
 
 	if err != nil {
 		return "", err
 	}
 
-	if claims, ok := token.Claims.(*jwtCustomClaims); ok && token.Valid {
-		return claims.UserID, nil
+	claims, ok := token.Claims.(CustomClaim)
+	if !ok || !token.Valid {
+		return "", errors.New("invalid token")
 	}
 
-	return "", errors.New("invalid token")
+	expiry, err := claims.GetExpirationTime()
+	if err != nil {
+		return "", errors.New("invalid token")
+	}
+
+	if time.Now().After(expiry.Time) {
+		return "", errors.New("token is expired")
+	}
+
+	userID := claims.UserID
+	if userID == "" {
+		return "", errors.New("invalid token")
+	}
+
+	return userID, nil
 }
